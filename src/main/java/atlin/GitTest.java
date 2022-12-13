b4a8606d5afcea385d1c372ea8181699050660ee
lin <2322954392@qq.com>
2022-12-13 15:10:41 +0800

    push commit

--- src/main/java/atlin/GitTest.java
+++ src/main/java/atlin/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello");
         System.out.println("hello git4,2");
+        System.out.println("hello git4,push");
     }
 }
