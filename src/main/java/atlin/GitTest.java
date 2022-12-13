87f75a0fe90fdc8f5de3e20c015da1019a5a0578
lin <2322954392@qq.com>
2022-12-13 14:43:36 +0800

    second commit

--- src/main/java/atlin/GitTest.java
+++ src/main/java/atlin/GitTest.java
@@ -10,6 +10,6 @@ package atlin;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 }
