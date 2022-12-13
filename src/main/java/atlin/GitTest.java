482e9ebc36e4fb817f5f2b6dd1971ae084a6689e
lin <2322954392@qq.com>
2022-12-13 14:40:44 +0800

    first commit

---
+++ src/main/java/atlin/GitTest.java
@@ -0,0 +1,15 @@
+package atlin;
+
+/**
+ * @author:Linhai
+ * @className:GitTest
+ * @description:
+ * @date:2022/12/1314:35
+ * @version:0.1
+ * @since:11.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
